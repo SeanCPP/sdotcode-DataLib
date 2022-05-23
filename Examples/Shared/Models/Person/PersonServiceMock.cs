dc630c732ea07d5b52d1636f8c31845de4f70308
Sean Patrick Franklin <sean.patrick516@gmail.com>
2022-05-22 22:06:07 -0400

    added more mock data

--- Examples/Shared/Models/Person/PersonServiceMock.cs
+++ Examples/Shared/Models/Person/PersonServiceMock.cs
@@ -13,6 +13,12 @@ public class PersonServiceMock : Service<PersonModel>
             new PersonModel { Id = 1, Name = "Curly" },
             new PersonModel { Id = 2, Name = "Larry" },
             new PersonModel { Id = 3, Name = "Moe" },
+            new PersonModel { Id = 4, Name = "Bill" },
+            new PersonModel { Id = 5, Name = "Ted" },
+            new PersonModel { Id = 6, Name = "Homer" },
+            new PersonModel { Id = 7, Name = "Lisa" },
+            new PersonModel { Id = 8, Name = "Marge" },
+            new PersonModel { Id = 9, Name = "Maggie" },
         });
     }
 }
