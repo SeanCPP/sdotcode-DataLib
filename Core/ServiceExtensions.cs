669389f7465785b393f952d6e73754d369ae5442
Sean Patrick Franklin <sean.patrick516@gmail.com>
2022-07-04 18:22:12 -0400

    Added new feature that allows automatic DI registration of all entities with one line of code

--- Core/ServiceExtensions.cs
+++ Core/ServiceExtensions.cs
@@ -3,6 +3,7 @@ using sdotcode.DataLib.Core.DataStores;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,15 @@ namespace sdotcode.DataLib.Core
 {
     public static class ServiceExtensions
     {
+        public static IServiceCollection AddWebApiRepository<TEntity, TServiceImpl>(this IServiceCollection services)
+            where TEntity : IStoredItem, new()
+            where TServiceImpl : Service<TEntity>
+        {
+            services.AddScoped<IDataStore<TEntity>, HttpClientDataStore<TEntity>>();
+            services.AddScoped<Service<TEntity>, TServiceImpl>();
+            return services;
+        }
+
         public static IServiceCollection AddWebApiRepositoryFor<TEntity>(this IServiceCollection services)
             where TEntity : IStoredItem, new()
         { 
@@ -18,12 +28,32 @@ namespace sdotcode.DataLib.Core
             return services;
         }
 
-        public static IServiceCollection AddWebApiRepository<TEntity, TServiceImpl>(this IServiceCollection services)
-            where TEntity : IStoredItem, new()
-            where TServiceImpl : Service<TEntity>
+        public static IServiceCollection AddWebApiRepositoryFor(this IServiceCollection services, 
+            string entitiesNamespace, 
+            IDictionary<Type, Type>? overrides = null)
         {
-            services.AddScoped<IDataStore<TEntity>, HttpClientDataStore<TEntity>>();
-            services.AddScoped<Service<TEntity>, TServiceImpl>();
+            overrides ??= new Dictionary<Type, Type>();
+
+            var assemblies = Assembly.GetCallingAssembly().GetReferencedAssemblies();
+            foreach(var assembly in assemblies)
+            {
+                var types = Assembly.Load(assembly).GetTypes().Where(type => type.Namespace == entitiesNamespace);
+                foreach (var type in types)
+                {
+                    if (overrides.ContainsKey(type))
+                    {
+                        continue;
+                    }
+                    services.AddScoped(typeof(IDataStore<>).MakeGenericType(type), typeof(HttpClientDataStore<>).MakeGenericType(type));
+                    services.AddScoped(typeof(Service<>).MakeGenericType(type));
+                }
+            }
+
+            foreach(var entityType in overrides.Keys)
+            {
+                services.AddScoped(typeof(IDataStore<>).MakeGenericType(entityType), typeof(HttpClientDataStore<>).MakeGenericType(entityType));
+                services.AddScoped(typeof(Service<>).MakeGenericType(entityType), overrides[entityType]);
+            }
             return services;
         }
     }
--- sdotcode.DataLib.Example.BlazorApp/Program.cs
+++ sdotcode.DataLib.Example.BlazorApp/Program.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using sdotcode.DataLib.Core;
+using sdotcode.DataLib.Core.DataStores;
 using sdotcode.DataLib.Example.BlazorApp;
 using sdotcode.DataLib.Examples;
-
+using sdotcode.DataLib.Examples.Entities;
+using System.Reflection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,6 +13,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7051/") });
 
-builder.Services.AddWebApiRepositoryFor<PersonModel>();
+//builder.Services.AddWebApiRepositoryFor<PersonModel>();
+
+builder.Services.AddWebApiRepositoryFor("sdotcode.DataLib.Examples.Entities");
+
+//builder.Services.AddWebApiRepositoryFor("sdotcode.DataLib.Examples.Entities", new Dictionary<Type, Type>
+//{
+//    [typeof(PersonModel)] = typeof(PersonServiceMock)
+//});
 
 await builder.Build().RunAsync();
